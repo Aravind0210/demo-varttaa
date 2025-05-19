d1910d25f878ca985f24b8c2352a2aabe6b32eb1
Aravind0210 <aravindmurugesan260@gmail.com>
2025-05-19 11:09:08 +0530

    rework

--- src/app/site/footer/footer.component.ts
+++ src/app/site/footer/footer.component.ts
@@ -1,16 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [RouterLink, CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  copied: boolean = false;
+
   copyToClipboard(text: string) {
     navigator.clipboard.writeText(text).then(() => {
+      this.copied = true;
       console.log(`${text} copied!`);
+
+      // Reset the copied state after 2 seconds
+      setTimeout(() => {
+        this.copied = false;
+      }, 2000);
     });
   }
 }
